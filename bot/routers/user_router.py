82ad2562f1ffc75775996a0a6c8322846f8b620c
Dmitry <omenharvest@gmail.com>
2025-11-08 04:48:21 +0300

    some dev updates

--- bot/routers/user_router.py
+++ bot/routers/user_router.py
@@ -1,4 +1,9 @@
 from aiogram import Router
-
+from aiogram.types import Message
+from aiogram.filters.command import Command, CommandStart
 
 user_handler = Router()
+
+@user_handler.message(CommandStart())
+async def cmd_start():
+    pass
