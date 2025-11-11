86c71758c4b64af488d7fc74780c22052faff4c6
Dmitry <omenharvest@gmail.com>
2025-11-11 11:08:44 +0300

    some dev updates
    -updated gitignore
    -updated startup files

--- bot/startup.py
+++ bot/startup.py
@@ -1,17 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+import logging
 
 from startup_properties import TOKEN_API
 
 from routers import admin_router, user_router
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(TOKEN_API)
 dispath = Dispatcher()
 
 dispath.include_routers(admin_router.admin_router, user_router.user_handler)
 
-async def start_bot():
+async def main():
     await dispath.start_polling(bot);
 
-if __name__ == "__name__":
-    asyncio.run(start_bot())
+if __name__ == "__main__":
+    asyncio.run(main())
--- bot/startup_properties.py
+++ bot/startup_properties.py
@@ -1,7 +1,8 @@
 import os
 import logging
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
 
+load_dotenv()
 TOKEN_API = os.getenv('TOKEN_API')
-
